0c42139528ba1d6aca944850c89c8d57b9b8d4d7
Devi Amaolo <deviamaolo@gmail.com>
2023-03-30 16:38:37 -0300

    create login ui component

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -5,7 +5,10 @@ const Login = () => {
     <main className={styles.loginMain}>
       <section className={styles.loginSection}>
         <div className={styles.loginContainer}>
-          <h1>Sign In</h1>
+          <div>
+            <h1>TaskToDo</h1>
+            <h2>Sign In</h2>
+          </div>
           <form className={styles.loginForm}>
             <div className={styles.inputContainer}>
               <label id='email'>User name</label>
@@ -15,7 +18,7 @@ const Login = () => {
               <label id='password'>Password</label>
               <input name='password' type='text' placeholder='type your password' />
             </div>
-            <button>Login</button>
+            <button className={styles.loginButton}>Login</button>
           </form>
           <p>Don't have a account yet? <span><a href='#'>Signup</a></span></p>
         </div>
